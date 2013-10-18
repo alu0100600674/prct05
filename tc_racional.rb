eb7f1d208713c2d3fa9513dbe501409974665675
Jonay Suárez <alu0100600674@ull.edu.es>
2013-10-18 23:48:20 +0100

    Creados los test

--- tc_racional.rb
+++ tc_racional.rb
@@ -5,4 +5,29 @@ require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
 
+	#Test de def
+	def test_simple
+		assert.equal("2/4", Fraccion.new(2, 4).to_s)
+	end
+
+	#Test para la suma
+	def test_suma
+		assert_equal("3/4", (Fraccion.new(1, 2) + Fraccion.new(1, 4)).to_s)
+	end
+
+	#Test para la resta
+	def test_resta
+		assert_equal("1/2", (Fraccion.new(3, 4) - Fraccion.new(1, 4)).to_s)
+	end
+
+	#Test para la multiplicacion
+	def test_producto
+		assert_equal("3/20", (Fraccion.new(2, 5) * Fraccion.new(3, 8)).to_s)
+	end
+
+	#Test para la division
+	def test_division
+		assert_equal("16/15", (Fraccion.new(2, 5) / Fraccion.new(3, 8)).to_s)
+	end
+
 end
