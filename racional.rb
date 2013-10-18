0e28a730d3c0ff22f33ca67eaaddedc1ba4186cb
Jonay Suárez <alu0100600674@ull.edu.es>
2013-10-18 22:37:15 +0100

    Métodos definidos

--- racional.rb
+++ racional.rb
@@ -3,4 +3,33 @@
 require "./gcd.rb"
 
 class Fraccion
+        #Constructor
+        def initialize(a, b)
+                @a, @b = a, b
+        end
+
+        #To_s
+        def to_s
+                "#{@a}/#{@b}"
+        end
+
+        #Suma
+        def +(other)
+                Fraccion.new(@a + other.a, @b + other.b)
+        end
+
+        #Resta
+        def -(other)
+                Fraccion.new(@a - other.b, @b - other.b)
+        end
+
+        #Producto
+        def *(value)
+                Fraccion.new(@a * value, @b * value)
+        end
+
+        #Division
+        def /(value)
+                Fraccion.new(@a, @b * value)
+        end
 end
